5df1b335946705bf833b39145a0ae95785bc3c01
dashluu <dashluu9121997@gmail.com>
2022-11-15 10:14:38 -0800

    Update grading script.

--- grade.sh
+++ grade.sh
@@ -1,7 +1,16 @@
-# Create your grading script here
-
 set -e
 
 rm -rf student-submission
 git clone $1 student-submission
-
+FILE=student-submission/ListExamples.java
+if [ ! -f "$FILE" ]; then
+ 	echo "File does not exist."
+fi
+CPATH=.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar
+set +e
+javac -cp $CPATH *.java 2>error.txt
+if [[ $? -ne 0 ]]; then
+	echo "Compiler error."
+	exit
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 
